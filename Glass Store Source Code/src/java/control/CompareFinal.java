bd95a4f58a842154ad328a10b2901b6df71c4ee2
Vu Minh <tissa0800@gmail.com>
2022-10-15 20:56:32 +0700

    test

---
+++ Glass Store Source Code/src/java/control/CompareFinal.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import entity.Category;
+import entity.Information;
+import entity.Product;
+import entity.ProductCompare;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.CategoryDAO;
+import model.InforDAO;
+import model.ProductCompareDAO;
+import model.ProductDAO;
+
+/**
+ *
+ * @author dell
+ */
+public class CompareFinal extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         response.setContentType("text/html;charset=UTF-8");
+
+        try {
+            HttpSession session = request.getSession();
+
+            //Getting id of the second product to be compared
+            String id1 = request.getParameter("id");
+            CategoryDAO CategoryDAO = new CategoryDAO();
+            //Getting id of the first product (which has already been stored in session)
+            String id2 = String.valueOf(session.getAttribute("productSession"));
+            List<Category> listC = CategoryDAO.getAllCategory(); //Get List Category
+
+            ProductDAO ProductDAO = new ProductDAO();
+            ProductCompareDAO ProductCompareDAO = new ProductCompareDAO();
+            InforDAO InforDAO = new InforDAO();
+
+            Product hot = ProductDAO.getHotProduct(); //Get First Product
+            Product favor = ProductDAO.getFavoriteProduct(); //Get Last Product
+            Information infor = InforDAO.getInfor(); //Get Information
+            ProductCompare product1 = ProductCompareDAO.getProductByID(id1); //Get the selected Product infor
+            ProductCompare product2 = ProductCompareDAO.getProductByID(id2); //Get the selected Product infor
+
+            //Seding data to jsp page
+            request.setAttribute("product1", product1);
+            request.setAttribute("product2", product2);
+            request.setAttribute("hot", hot);
+            request.setAttribute("favor", favor);
+            request.setAttribute("infor", infor);
+            request.setAttribute("allCategory", listC);
+
+            /*
+        Sending data to final comparing page with 2 chosen product's details
+             */
+            request.getRequestDispatcher("CompareFinal.jsp").forward(request, response);
+        } catch (Exception e) {
+            response.sendRedirect("Error.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- Glass Store Source Code/src/java/control/ProductListController.java
+++ Glass Store Source Code/src/java/control/ProductListController.java
@@ -8,7 +8,11 @@
 
 package control;
 
+import model.InforDAO;
+import entity.Account;
 import entity.Category;
+import entity.Information;
+import entity.Notification;
 import entity.Product;
 import java.io.IOException;
 import java.util.List;
@@ -18,6 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.CategoryDAO;
+import model.NotificationDAO;
 import model.ProductDAO;
 
 /**
@@ -37,21 +42,22 @@ public class ProductListController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         response.setContentType("text/html;charset=UTF-8");
+           
+        response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
-        
+        try {
             //Call to DAOs
 
             HttpSession session = request.getSession();
             if (session.getAttribute("acc") == null) {
                 ProductDAO ProductDAO = new ProductDAO();
-               // InforDAO InforDAO = new InforDAO();
+                InforDAO InforDAO = new InforDAO();
                 CategoryDAO CategoryDAO = new CategoryDAO();
 
                 List<Category> listC = CategoryDAO.getAllCategory(); //Get List Category
                 Product hot = ProductDAO.getHotProduct(); //Get First Product
                 Product favor = ProductDAO.getFavoriteProduct(); //Get Last Product
-        
+                Information infor = InforDAO.getInfor(); //Get Information
 
 
                 //Paging By CategoryID
@@ -88,7 +94,7 @@ public class ProductListController extends HttpServlet {
                 request.setAttribute("allCategory", listC);
                 request.setAttribute("hot", hot);
                 request.setAttribute("favor", favor);
-            
+                request.setAttribute("infor", infor);
 
                 request.setAttribute("listP", list); //List Product
                 request.setAttribute("end", endPage);
@@ -98,9 +104,76 @@ public class ProductListController extends HttpServlet {
                 request.setAttribute("CateName", CategoryDAO.getCateNameByID(CID));
 
                 request.getRequestDispatcher("productList.jsp").forward(request, response);
-            
+            } else {
+                ProductDAO ProductDAO = new ProductDAO();
+                InforDAO InforDAO = new InforDAO();
+                CategoryDAO CategoryDAO = new CategoryDAO();
+                NotificationDAO notiDAO = new NotificationDAO();
+                Account user = (Account) session.getAttribute("acc");
 
-    }}
+                int userId = user.getId();
+                int unreadNotifications = notiDAO.countUnreadNotifications(userId);
+                List<Category> listC = CategoryDAO.getAllCategory(); //Get List Category
+                Product hot = ProductDAO.getHotProduct(); //Get First Product
+                Product favor = ProductDAO.getFavoriteProduct(); //Get Last Product
+                Information infor = InforDAO.getInfor(); //Get Information
+                List<Notification> notis = notiDAO.getTop5NotificationsByUserID(userId);
+                int notiss = notiDAO.countNotifications(userId);
+
+                //Paging By CategoryID
+                String CategoryID = request.getParameter("CategoryID");
+                if (CategoryID == null) { //On Load: User hasn't choosen Category
+                    CategoryID = "0";
+                }
+                //Set Category ID back on JSP
+                request.setAttribute("CategoryID", CategoryID);
+
+                int CID = Integer.parseInt(CategoryID);
+
+                //Get Page number from JSP
+                String indexPage = request.getParameter("index");
+                if (indexPage == null) {
+                    //On load: Page 1
+                    indexPage = "1";
+                }
+
+                int index = Integer.parseInt(indexPage);
+
+                //Count number of Product According to the Category -> Number of Pages
+                int count = ProductDAO.countProductByCategory(CID);
+                int endPage = count / 6;
+                if (count % 6 != 0) {
+                    //If the number of Product isn't divided by 3 -> Need 1 more Page
+                    endPage++;
+                }
+
+                //List of Product to Display after Paging by Category ID
+                List<Product> list = ProductDAO.pagingByCategory(index, CID);
+
+                //Set Data to JSP
+                request.setAttribute("allCategory", listC);
+                request.setAttribute("hot", hot);
+                request.setAttribute("favor", favor);
+                request.setAttribute("infor", infor);
+
+                request.setAttribute("listP", list); //List Product
+                request.setAttribute("end", endPage);
+                request.setAttribute("unread", unreadNotifications);
+                request.setAttribute("notis", notis);
+                request.setAttribute("numberOfNotifications", notiss);
+                request.setAttribute("tag", index); //Page number
+                request.setAttribute("count", count);
+                request.setAttribute("CateID", CID);
+                request.setAttribute("acc",user);
+                request.setAttribute("CateName", CategoryDAO.getCateNameByID(CID));
+
+                request.getRequestDispatcher("productList.jsp").forward(request, response);
+            }
+        } catch (Exception e) {
+            response.sendRedirect("Error.jsp");
+        }
+
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
