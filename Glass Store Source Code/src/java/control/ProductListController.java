0eb5a6da99382bf2ca1827a6e9e3124d834b3bb3
duyhung2h <duyhung2h@gmail.com>
2022-10-16 07:26:14 +0700

    sua conflict productlistcontroller

--- Glass Store Source Code/src/java/control/ProductListController.java
+++ Glass Store Source Code/src/java/control/ProductListController.java
@@ -34,170 +34,65 @@ public class ProductListController extends HttpServlet {
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-<<<<<<< HEAD
-           
         response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
-        try {
-            //Call to DAOs
-=======
-        response.setContentType("text/html;charset=UTF-8");
-        request.setCharacterEncoding("UTF-8");
->>>>>>> 36d7dfd0fc2827c8f1d547dcd83ebb85dc571853
 
-        //Call to DAOs
+        // Call to DAOs
         try {
             HttpSession session = request.getSession();
-<<<<<<< HEAD
-            if (session.getAttribute("acc") == null) {
-                ProductDAO ProductDAO = new ProductDAO();
-                InforDAO InforDAO = new InforDAO();
-                CategoryDAO CategoryDAO = new CategoryDAO();
-
-                List<Category> listC = CategoryDAO.getAllCategory(); //Get List Category
-                Product hot = ProductDAO.getHotProduct(); //Get First Product
-                Product favor = ProductDAO.getFavoriteProduct(); //Get Last Product
-                Information infor = InforDAO.getInfor(); //Get Information
-=======
             ProductDAO ProductDAO = new ProductDAO();
             // InforDAO InforDAO = new InforDAO();
             CategoryDAO CategoryDAO = new CategoryDAO();
 
-            List<Category> listC = CategoryDAO.getAllCategory(); //Get List Category
-            Product hot = ProductDAO.getHotProduct(); //Get First Product
-            Product favor = ProductDAO.getFavoriteProduct(); //Get Last Product
->>>>>>> 36d7dfd0fc2827c8f1d547dcd83ebb85dc571853
+            List<Category> listC = CategoryDAO.getAllCategory(); // Get List Category
+            Product hot = ProductDAO.getHotProduct(); // Get First Product
+            Product favor = ProductDAO.getFavoriteProduct(); // Get Last Product
 
-            //Paging By CategoryID
+            // Paging By CategoryID
             String CategoryID = request.getParameter("CategoryID");
-            if (CategoryID == null) { //On Load: User hasn't choosen Category
+            if (CategoryID == null) { // On Load: User hasn't choosen Category
                 CategoryID = "0";
             }
-            //Set Category ID back on JSP
+            // Set Category ID back on JSP
             request.setAttribute("CategoryID", CategoryID);
 
             int CID = Integer.parseInt(CategoryID);
 
-            //Get Page number from JSP
+            // Get Page number from JSP
             String indexPage = request.getParameter("index");
             if (indexPage == null) {
-                //On load: Page 1
+                // On load: Page 1
                 indexPage = "1";
             }
 
             int index = Integer.parseInt(indexPage);
 
-            //Count number of Product According to the Category -> Number of Pages
+            // Count number of Product According to the Category -> Number of Pages
             int count = ProductDAO.countProductByCategory(CID);
             int endPage = count / 6;
             if (count % 6 != 0) {
-                //If the number of Product isn't divided by 3 -> Need 1 more Page
+                // If the number of Product isn't divided by 3 -> Need 1 more Page
                 endPage++;
             }
 
-            //List of Product to Display after Paging by Category ID
+            // List of Product to Display after Paging by Category ID
             List<Product> list = ProductDAO.pagingByCategory(index, CID);
 
-            //Set Data to JSP
+            // Set Data to JSP
             request.setAttribute("allCategory", listC);
             request.setAttribute("hot", hot);
             request.setAttribute("favor", favor);
 
-<<<<<<< HEAD
-                //Set Data to JSP
-                request.setAttribute("allCategory", listC);
-                request.setAttribute("hot", hot);
-                request.setAttribute("favor", favor);
-                request.setAttribute("infor", infor);
-
-                request.setAttribute("listP", list); //List Product
-                request.setAttribute("end", endPage);
-                request.setAttribute("tag", index); //Page number
-                request.setAttribute("count", count);
-                request.setAttribute("CateID", CID);
-                request.setAttribute("CateName", CategoryDAO.getCateNameByID(CID));
-
-                request.getRequestDispatcher("productList.jsp").forward(request, response);
-            } else {
-                ProductDAO ProductDAO = new ProductDAO();
-                InforDAO InforDAO = new InforDAO();
-                CategoryDAO CategoryDAO = new CategoryDAO();
-                NotificationDAO notiDAO = new NotificationDAO();
-                Account user = (Account) session.getAttribute("acc");
-
-                int userId = user.getId();
-                int unreadNotifications = notiDAO.countUnreadNotifications(userId);
-                List<Category> listC = CategoryDAO.getAllCategory(); //Get List Category
-                Product hot = ProductDAO.getHotProduct(); //Get First Product
-                Product favor = ProductDAO.getFavoriteProduct(); //Get Last Product
-                Information infor = InforDAO.getInfor(); //Get Information
-                List<Notification> notis = notiDAO.getTop5NotificationsByUserID(userId);
-                int notiss = notiDAO.countNotifications(userId);
-
-                //Paging By CategoryID
-                String CategoryID = request.getParameter("CategoryID");
-                if (CategoryID == null) { //On Load: User hasn't choosen Category
-                    CategoryID = "0";
-                }
-                //Set Category ID back on JSP
-                request.setAttribute("CategoryID", CategoryID);
-
-                int CID = Integer.parseInt(CategoryID);
-
-                //Get Page number from JSP
-                String indexPage = request.getParameter("index");
-                if (indexPage == null) {
-                    //On load: Page 1
-                    indexPage = "1";
-                }
-
-                int index = Integer.parseInt(indexPage);
-
-                //Count number of Product According to the Category -> Number of Pages
-                int count = ProductDAO.countProductByCategory(CID);
-                int endPage = count / 6;
-                if (count % 6 != 0) {
-                    //If the number of Product isn't divided by 3 -> Need 1 more Page
-                    endPage++;
-                }
-
-                //List of Product to Display after Paging by Category ID
-                List<Product> list = ProductDAO.pagingByCategory(index, CID);
-
-                //Set Data to JSP
-                request.setAttribute("allCategory", listC);
-                request.setAttribute("hot", hot);
-                request.setAttribute("favor", favor);
-                request.setAttribute("infor", infor);
-
-                request.setAttribute("listP", list); //List Product
-                request.setAttribute("end", endPage);
-                request.setAttribute("unread", unreadNotifications);
-                request.setAttribute("notis", notis);
-                request.setAttribute("numberOfNotifications", notiss);
-                request.setAttribute("tag", index); //Page number
-                request.setAttribute("count", count);
-                request.setAttribute("CateID", CID);
-                request.setAttribute("acc",user);
-                request.setAttribute("CateName", CategoryDAO.getCateNameByID(CID));
-
-                request.getRequestDispatcher("productList.jsp").forward(request, response);
-            }
-        } catch (Exception e) {
-            response.sendRedirect("Error.jsp");
-        }
-
-=======
-            request.setAttribute("listP", list); //List Product
+            request.setAttribute("listP", list); // List Product
             request.setAttribute("end", endPage);
-            request.setAttribute("tag", index); //Page number
+            request.setAttribute("tag", index); // Page number
             request.setAttribute("count", count);
             request.setAttribute("CateID", CID);
             request.setAttribute("CateName", CategoryDAO.getCateNameByID(CID));
@@ -207,17 +102,17 @@ public class ProductListController extends HttpServlet {
             e.printStackTrace();
         } finally {
         }
->>>>>>> 36d7dfd0fc2827c8f1d547dcd83ebb85dc571853
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+    // + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -228,10 +123,10 @@ public class ProductListController extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
